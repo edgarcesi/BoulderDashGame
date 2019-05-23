d05f522fdb0977684d5d2650c3747c0d29470163
Antoine <antoine.caputo1@viacesi.fr>
2019-05-23 09:45:23 +0200

    clean

---
+++ contract/src/main/java/contract/ControllerOrder.java
@@ -0,0 +1,19 @@
+package contract;
+
+/**
+ * The Enum ControllerOrder.
+ *
+ * @author Jean-Aymeric Diet
+ */
+public enum ControllerOrder {
+	/** The English. */
+	English,
+	/** The French. */
+	Francais,
+	/** The Deutch. */
+	Deutsch,
+	/** The Italian. */
+	Italian,
+	/** The Spain. */
+	Spanish
+}
