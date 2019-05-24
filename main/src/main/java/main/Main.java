24d4270aa323ac58bd095940a5652423e7065b90
Antoine <antoine.caputo1@viacesi.fr>
2019-05-24 09:54:48 +0200

    Create player class

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -29,7 +29,7 @@ public abstract class Main {
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
         view.setController(controller);
-//test
+
         controller.orderPerform(ControllerOrder.English);
     }
 }
